4ab99dc35f28667bdaf48febf8f539e3805851c4
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-24 17:55:58 -0300

    ADD (functions) handlers to all omp's

--- q6/omp_for.c
+++ q6/omp_for.c
@@ -1,31 +1,30 @@
-#include "common.h"
+#include "omp_dynamic.h"
+#include "omp_guideline.h"
+#include "omp_static.h"
 
 void omp_for(int start, int step, int final, int schedule, int chunk_size,
              void (*f)(int)) {
 
   pthread_t *tArr;
   tuple *tupleArr;
-  function_args omp_args = {start,
-                            step,
-                            final,
-                            schedule,
-                            chunk_size,
-                            (void *)f,
-                            generate_static_sched((void *)&omp_args)};
+  function_args omp_args = {start,    step,       final,
+                            schedule, chunk_size, (void *)f};
 
-  pos = start;
   pthread_mutex_init(&omp_mut, NULL);
   switch (schedule) {
   case STATIC:
+    omp_args.workSchedule = generate_static_sched((void *)&omp_args);
     handler_static(&omp_args, &tArr, &tupleArr);
     break;
 
   case DYNAMIC:
-    handler_static(&omp_args, &tArr, &tupleArr);
+    omp_args.workSchedule = generate_dynamic_sched((void *)&omp_args);
+    handler_dynamic(&omp_args, &tArr, &tupleArr);
     break;
 
   case GUIDELINE:
-    handler_static(&omp_args, &tArr, &tupleArr);
+    omp_args.workSchedule = generate_guideline_sched((void *)&omp_args);
+    handler_guideline(&omp_args, &tArr, &tupleArr);
     break;
   }
 }
@@ -36,10 +35,10 @@ void OK(int i) {
 }
 
 int main(void) {
-  /* omp_for(0, 1, 50, GUIDELINE, 2, OK); */
-  /* counter = 0; */
+  omp_for(0, 1, 50, GUIDELINE, 2, OK);
+  counter = 0;
   /* omp_for(0, 1, 20, DYNAMIC, 1, OK); */
   /* printf("%d", counter); */
-  omp_for(0, 1, 51, STATIC, 3, OK);
-  counter = 0;
+  /* omp_for(0, 1, 51, STATIC, 3, OK); */
+  /* counter = 0; */
 }
